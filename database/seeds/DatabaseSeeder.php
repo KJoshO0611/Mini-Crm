660d93fabf460566d91c1f4a2e35270437bbb6e7
KJoshO0611 <jeslagam@gmail.com>
2019-09-21 19:54:03 +0800

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(CompaniesSeeder::class);
-        $this->call(EmployeeSeeder::class);
+        //$this->call(CompaniesSeeder::class);
+        //$this->call(EmployeeSeeder::class);
     }
 }
